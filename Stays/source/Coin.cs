179013eb0cea6576e8aeb10bd7c5988509c0e83f
Denis.Golikov@urfu.me <Denis.Golikov@urfu.me>
2023-05-21 16:58:16 +0500

    seventh push

--- Stays/source/Coin.cs
+++ Stays/source/Coin.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stays.source;
 
 namespace Stays.source
 {
-    internal class Coin
+    public class Coin
     {
+        private Animation coinAnimation;
+        public Coin(Texture2D sprite)
+        {
+            coinAnimation = new Animation(sprite, 16, 16);
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, GameTime gameTime)
+        {
+            coinAnimation.Draw(spriteBatch, position, gameTime);
+        }
     }
 }
